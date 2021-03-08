bfc8bb2623d81f2e05f0439b0974135b7b80a0c1
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-03-08 16:34:28 -0600

    5th commit

--- src/main/java/ArrayStats.java
+++ src/main/java/ArrayStats.java
@@ -11,17 +11,34 @@
 public class ArrayStats {
     public static void main(String[] args) {
         int [] TestData = {1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20};
+        int Total = getTotal(TestData);
+        double Average = getAverage(TestData);
+        int Highest = getHighest(TestData);
+        int Lowest = getLowest(TestData);
         
-        
-    }
+        System.out.println("The total of the values in the array is " + Total);
+        System.out.println("The average of the values in the array is " + Average);
+        System.out.println("The highest value in the array is " + Highest);
+        System.out.println("The lowest values in the array is " + Lowest);
+    } 
     
     public static int getTotal(int [] Array){
-        
+        int total = 0;
+        for (int i = 1; i < Array.length; i++){
+            total += Array[i];
+        }
+        return total;
+        }
+                
      
-    }
-     public static int getAverage(int [] Array){
-         
-         
+    
+     public static double getAverage(int [] Array){
+        int total = 0;
+        for (int i = 1; i < Array.length; i++){
+            total += Array[i];
+        }
+        double average = total / Array.length;
+        return average;
     }
      
       public static int getHighest(int [] Array){
